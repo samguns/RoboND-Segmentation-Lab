fb6d887e422ee504005bbdd569ab43c9f7a7cf60
samguns <wang.vai@gmail.com>
2018-10-17 15:54:14 +0800

    Initial draft.

---
+++ code/seg.py
@@ -0,0 +1,130 @@
+import os
+import glob
+import sys
+import tensorflow as tf
+
+from scipy import misc
+import numpy as np
+
+from tensorflow.contrib.keras.python import keras
+from tensorflow.contrib.keras.python.keras import layers, models
+
+from tensorflow import image
+
+from utils import scoring_utils
+from utils.separable_conv2d import SeparableConv2DKeras, BilinearUpSampling2D
+from utils import data_iterator
+from utils import plotting_tools
+from utils import model_tools
+
+
+def separable_conv2d_batchnorm(input_layer, filters, strides=1):
+    output_layer = SeparableConv2DKeras(filters=filters, kernel_size=3, strides=strides,
+                                        padding='same', activation='relu')(input_layer)
+    output_layer = layers.BatchNormalization()(output_layer)
+    return output_layer
+
+
+def conv2d_batchnorm(input_layer, filters, kernel_size=3, strides=1):
+    output_layer = layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides,
+                                 padding='same', activation='relu')(input_layer)
+    output_layer = layers.BatchNormalization()(output_layer)
+    return output_layer
+
+
+def bilinear_upsample(input_layer):
+    output_layer = BilinearUpSampling2D((2,2))(input_layer)
+    return output_layer
+
+
+def encoder_block(input_layer, filters, strides):
+    # Create a separable convolution layer using the separable_conv2d_batchnorm() function.
+    output_layer = separable_conv2d_batchnorm(input_layer, filters, strides)
+    return output_layer
+
+
+def decoder_block(small_ip_layer, large_ip_layer, filters):
+    # Upsample the small input layer using the bilinear_upsample() function.
+    output_layer = bilinear_upsample(small_ip_layer)
+    # Concatenate the upsampled and large input layers using layers.concatenate
+    output_layer = layers.concatenate([output_layer, large_ip_layer])
+    # Add some number of separable convolution layers
+    output_layer = separable_conv2d_batchnorm(output_layer, filters)
+    output_layer = separable_conv2d_batchnorm(output_layer, filters)
+    return output_layer
+
+
+def fcn_model(inputs, num_classes):
+    # Add Encoder Blocks.
+    # Remember that with each encoder layer, the depth of your model (the number of filters) increases.
+    skip_0 = inputs
+    x = encoder_block(inputs, 64, 2)
+    skip_1 = x
+    x = encoder_block(x, 128, 2)
+    skip_2 = x
+    x = encoder_block(x, 256, 2)
+    skip_3 = x
+    x = encoder_block(x, 512, 2)
+
+    # Add 1x1 Convolution layer using conv2d_batchnorm().
+    x = conv2d_batchnorm(x, 512, kernel_size=1, strides=1)
+
+    # Add the same number of Decoder Blocks as the number of Encoder Blocks
+    x = decoder_block(x, skip_3, 512)
+    x = decoder_block(x, skip_2, 256)
+    x = decoder_block(x, skip_1, 128)
+    x = decoder_block(x, skip_0, 64)
+
+    # The function returns the output layer of your model. "x" is the final layer obtained from the last decoder_block()
+    return layers.Conv2D(num_classes, 3, activation='softmax', padding='same')(x)
+
+
+image_hw = 128
+image_shape = (image_hw, image_hw, 3)
+inputs = layers.Input(image_shape)
+num_classes = 3
+
+output_layer = fcn_model(inputs, num_classes)
+
+learning_rate = 0.0001
+batch_size = 5
+num_epochs = 5
+steps_per_epoch = 10
+validation_steps = 10
+workers = 2
+
+# Define the Keras model and compile it for training
+model = models.Model(inputs=inputs, outputs=output_layer)
+
+model.compile(optimizer=keras.optimizers.Adam(learning_rate), loss='categorical_crossentropy')
+
+# Data iterators for loading the training and validation data
+train_iter = data_iterator.BatchIteratorSimple(batch_size=batch_size,
+                                               data_folder=os.path.join('..', 'data', 'train'),
+                                               image_shape=image_shape,
+                                               shift_aug=True)
+
+val_iter = data_iterator.BatchIteratorSimple(batch_size=batch_size,
+                                             data_folder=os.path.join('..', 'data', 'validation'),
+                                             image_shape=image_shape)
+
+logger_cb = plotting_tools.LoggerPlotter()
+callbacks = [logger_cb]
+
+model.fit_generator(train_iter,
+                    steps_per_epoch = steps_per_epoch, # the number of batches per epoch,
+                    epochs = num_epochs, # the number of epochs to train for,
+                    validation_data = val_iter, # validation iterator
+                    validation_steps = validation_steps, # the number of batches to validate on
+                    callbacks=callbacks,
+                    workers = workers)
+
+
+weight_file_name = 'model_weights'
+model_tools.save_network(model, weight_file_name)
+
+# generate predictions, save in the runs, directory.
+run_number = 'run1'
+validation_path, output_path = model_tools.write_predictions_grade_set(model,run_number,'validation')
+
+scoring_utils.score_run(validation_path, output_path)
